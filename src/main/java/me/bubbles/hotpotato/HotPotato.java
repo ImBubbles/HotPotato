5a5baa795d033e324b072cfb9c0959e76ed664c6
ImBubbles <bubblybubblesbubblesup@gmail.com>
2023-07-03 15:05:09 -0500

    Removed unneeded name variable

--- src/main/java/me/bubbles/hotpotato/HotPotato.java
+++ src/main/java/me/bubbles/hotpotato/HotPotato.java
@@ -20,7 +20,6 @@ public final class HotPotato extends JavaPlugin {
     private UserManager userManager;
     private Ticker ticker;
     private Messages messages;
-    private String name="hotpotato";
 
     @Override
     public void onEnable() {
@@ -67,9 +66,6 @@ public final class HotPotato extends JavaPlugin {
     }
 
     // GETTERS
-    public String getPluginName() {
-        return name;
-    }
     public CommandManager getCommandManager() {
         return commandManager;
     }
